892eb6f840879213d5d87b2b64965beaa8ca6b4e
Carlos Monteiro <85devs@gmail.com>
2024-08-23 22:30:57 -0300

    adicionar sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
