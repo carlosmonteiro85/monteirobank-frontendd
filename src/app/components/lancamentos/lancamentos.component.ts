5f53830886f8a0dbce93ad6a9c19a9aa1e1b2bd0
Carlos Monteiro <85devs@gmail.com>
2024-10-06 02:47:48 -0300

    categoria

--- src/app/components/lancamentos/lancamentos.component.ts
+++ src/app/components/lancamentos/lancamentos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/models/categoria';
+import { DominiosService } from 'src/app/service/dominios.service';
 
 @Component({
   selector: 'app-lancamentos',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lancamentos.component.css']
 })
 export class LancamentosComponent implements OnInit {
+  categorias: Categoria[] = []; // Propriedade para armazenar categorias
+  selectedCategoria: number | null = null; // Para armazenar a categoria selecionada
+  tipoLancamento: number | null = null;
 
-  constructor() { }
+  constructor(private dominiosService: DominiosService) { }
+
+  defaulTipoLancamento: string = "0";
 
   ngOnInit(): void {
+    // this.tipoLancamento.  .setValue(this.defaulTipoLancamento); 
+  }
+
+  onTipoLancamentoChange(): void {
+    if (this.tipoLancamento) {
+      this.loadCategorias(this.tipoLancamento); // Chama o método para carregar categorias
+    } else {
+      this.categorias = []; // Reseta as categorias se nenhum tipo for selecionado
+    }
+  }
+
+  loadCategorias(tipoCategoria: number): void {
+    this.dominiosService.getCategorias(tipoCategoria).subscribe(
+      (response) => {
+        this.categorias = response; // Armazena as categorias retornadas
+        this.selectedCategoria = null; // Reseta a categoria selecionada
+      },
+      (error) => {
+        console.error('Erro ao carregar categorias', error);
+        this.categorias = []; // Reseta as categorias em caso de erro
+      }
+    );
   }
 
 }
---
+++ src/app/service/dominios.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Categoria } from '../models/categoria';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DominiosService {
+
+  constructor(private http: HttpClient) { }
+
+  getCategorias(tipoCategoria: number) {
+    return this.http.get<Categoria[]>(`${API_CONFIG.baseUrl}/dominios/${tipoCategoria}`);
+  }
+}
